394f9021da4231d30ed13eeca48ec9e7ba6884ee
Procoder16 <smsoumik1608@gmail.com>
2021-06-14 17:23:51 +0530

    documentation added

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -6,8 +6,8 @@ int main(){
     int arr[] = {8,5,7,3,2};
     int n = 5;
 
-    for(int i = 0; i< n-1; i++){
-        for(int j = 0; j < n-1-i; j++){
+    for(int i = 0; i< n-1; i++){     // for n elements, (n-1) comparisons are done 
+        for(int j = 0; j < n-1-i; j++){  // for every i, the number of comparisons reduces by i
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
